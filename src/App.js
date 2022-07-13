35843321311cdd756a919bf734399656e808d459
Aditya <mishraadi733@gmail.com>
2022-07-13 22:01:16 +0530

    gym commited

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { Box } from "@mui/material";
+import {Routes, Route} from "react-router-dom";
 
-function App() {
+import React from 'react';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import ExerciseDetail from './pages/ExerciseDetail';
+import Footer from './components/Footer';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Box width="400px" sx={{width: {xl:"1488px"}}} m="auto" >
+      <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home /> } />
+        <Route path="/exercise/:id" element={<ExerciseDetail /> } />
+      </Routes>
+      <Footer/>
+
+    </Box>
+  )
 }
 
-export default App;
+export default App
+
+
